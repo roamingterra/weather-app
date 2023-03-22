ffc55b037d246f597a0663bd26bd36b70ab16739
roamingterra <danielballerini1@gmail.com>
2023-03-22 16:50:43 -0400

    Set up input form and event handler to return data
    In the previous commit, Each function that processes the weather api
    data did not return anything, they only logged to the console. I changed
    this so that the functions return a promises with objects and arrays of
    objects as the resolved data. These objects contain all of the required,
    processed data. I also created an event handler function that invokes
    all of the data processing functions, and logs to the console an array
    of this data once all promises are resolved. Finally, I added a simple
    input form in the html, so that the user can search for a city and its
    weather.

--- script.js
+++ script.js
@@ -7,28 +7,42 @@ async function getWeatherToday(location) {
     const json = await data.json();
     const locationAPI = json.location.name;
     const condition = json.current.condition.text;
-    const temperatureCelsius = json.current.temp_c;
-    const temperatureFahrenheit = json.current.temp_f;
-    const feelsLike_c = json.current.feelslike_c;
-    const feelsLike_f = json.current.feelslike_f;
+    const temperatureCelsius = Math.round(json.current.temp_c);
+    const temperatureFahrenheit = Math.round(json.current.temp_f);
+    const feelsLike_c = Math.round(json.current.feelslike_c);
+    const feelsLike_f = Math.round(json.current.feelslike_f);
     const precipitation_mm = json.current.precip_mm;
     const precipitation_in = json.current.precip_in;
     const humidity = json.current.humidity;
     const wind_kph = json.current.wind_kph;
     const wind_mph = json.current.wind_mph;
 
-    console.log(`WEATHER TODAY`);
-    console.log(locationAPI);
-    console.log(condition);
-    console.log(`Temperature: ${temperatureCelsius}°C`);
-    console.log(`Temperature: ${temperatureFahrenheit}°F`);
-    console.log(`Feels like: ${feelsLike_c}°C`);
-    console.log(`Feels like: ${feelsLike_f}°F`);
-    console.log(`Precipitation: ${precipitation_mm}mm`);
-    console.log(`Precipitation: ${precipitation_in}in`);
-    console.log(`Humidity: ${humidity}%`);
-    console.log(`Wind: ${wind_kph}km/h`);
-    console.log(`Wind: ${wind_mph}Mi/h`);
+    // console.log(`WEATHER TODAY`);
+    // console.log(locationAPI);
+    // console.log(condition);
+    // console.log(`Temperature: ${temperatureCelsius}°C`);
+    // console.log(`Temperature: ${temperatureFahrenheit}°F`);
+    // console.log(`Feels like: ${feelsLike_c}°C`);
+    // console.log(`Feels like: ${feelsLike_f}°F`);
+    // console.log(`Precipitation: ${precipitation_mm}mm`);
+    // console.log(`Precipitation: ${precipitation_in}in`);
+    // console.log(`Humidity: ${humidity}%`);
+    // console.log(`Wind: ${wind_kph}km/h`);
+    // console.log(`Wind: ${wind_mph}Mi/h`);
+
+    return {
+      location: locationAPI,
+      condition: condition,
+      temperatureCelsius: temperatureCelsius,
+      temperatureFahrenheit: temperatureFahrenheit,
+      feelsLikeCelsius: feelsLike_c,
+      feelsLikeFahrenheit: feelsLike_f,
+      precipitation_mm: precipitation_mm,
+      precipitation_in: precipitation_in,
+      humidity: humidity,
+      wind_kph: wind_kph,
+      wind_mph: wind_mph,
+    };
   } catch (error) {
     console.log(`${error}`);
     console.log("Could not find the searched location");
@@ -50,13 +64,15 @@ async function getWeatherForecastedTrihoral(location) {
     //Temperature forecast of every three hours (starting from the hour that is closest to the current time)
     const nextHour = currentHour + 1;
 
-    console.log("WEATHER FORECAST TRIHORAL FOR THE NEXT 22 HOURS");
+    // console.log("WEATHER FORECAST TRIHORAL FOR THE NEXT 22 HOURS");
 
+    const weatherEveryThreeHoursLibrary = [];
     for (
       let i = 0, forecastedHour = nextHour, currentDay = true;
       i < 8;
       i++, forecastedHour += 3
     ) {
+      // Convert time to AM/PM syntax
       let forecastedHourAmPmConverted;
       if (forecastedHour > 12 && forecastedHour < 24) {
         forecastedHourAmPmConverted = `${forecastedHour - 12} p.m.`;
@@ -67,24 +83,43 @@ async function getWeatherForecastedTrihoral(location) {
       } else {
         forecastedHourAmPmConverted = `${forecastedHour} a.m.`;
       }
-      if (currentDay) {
-        console.log(
-          `${forecastedHourAmPmConverted}: ${Math.round(
-            json.forecast.forecastday[0].hour[forecastedHour].temp_c
-          )}°C, ${Math.round(
-            json.forecast.forecastday[0].hour[forecastedHour].temp_f
-          )}°F`
-        );
-      } else {
-        console.log(
-          `${forecastedHourAmPmConverted}: ${Math.round(
-            json.forecast.forecastday[1].hour[forecastedHour].temp_c
-          )}°C, ${Math.round(
-            json.forecast.forecastday[1].hour[forecastedHour].temp_f
-          )}°F`
-        );
-      }
+
+      // Log to console
+      //   if (currentDay) {
+      //     console.log(
+      //       `${forecastedHourAmPmConverted}: ${Math.round(
+      //         json.forecast.forecastday[0].hour[forecastedHour].temp_c
+      //       )}°C, ${Math.round(
+      //         json.forecast.forecastday[0].hour[forecastedHour].temp_f
+      //       )}°F`
+      //     );
+      //   } else {
+      //     console.log(
+      //       `${forecastedHourAmPmConverted}: ${Math.round(
+      //         json.forecast.forecastday[1].hour[forecastedHour].temp_c
+      //       )}°C, ${Math.round(
+      //         json.forecast.forecastday[1].hour[forecastedHour].temp_f
+      //       )}°F`
+      //     );
+      //   }
+
+      // Add weather for the current hour to the array of objects
+      const obj = {};
+
+      obj[`Time of Day`] = forecastedHourAmPmConverted;
+
+      obj[`Temp Celsius`] = Math.round(
+        json.forecast.forecastday[0].hour[forecastedHour].temp_c
+      );
+
+      obj[`Temp Fahrenheit`] = Math.round(
+        json.forecast.forecastday[0].hour[forecastedHour].temp_f
+      );
+
+      weatherEveryThreeHoursLibrary.push(obj);
     }
+
+    return weatherEveryThreeHoursLibrary;
   } catch (error) {
     console.log(`${error}`);
     console.log("Could not find the searched location");
@@ -100,7 +135,9 @@ async function getWeatherForecastedDay(location) {
     );
     const json = await data.json();
 
-    console.log("WEATHER FORECASTED DAILY FOR THE NEXT 7 DAYS");
+    // console.log("WEATHER FORECASTED DAILY FOR THE NEXT 7 DAYS");
+
+    const WeatherDailyLibrary = [];
     for (let i = 0; i < 8; i++) {
       // Get day of the week
       const currentDateArray = json.forecast.forecastday[i].date.split("-");
@@ -112,23 +149,61 @@ async function getWeatherForecastedDay(location) {
       });
 
       // Get forecasted temp of current day (high and low for both celsius and fahrenheit)
-      const maxTempCelsius = json.forecast.forecastday[i].day.maxtemp_c;
-      const minTempCelsius = json.forecast.forecastday[i].day.mintemp_c;
-      const maxTempFahrenheit = json.forecast.forecastday[i].day.maxtemp_f;
-      const minTempFahrenheit = json.forecast.forecastday[i].day.mintemp_f;
+      const maxTempCelsius = Math.round(
+        json.forecast.forecastday[i].day.maxtemp_c
+      );
+      const minTempCelsius = Math.round(
+        json.forecast.forecastday[i].day.mintemp_c
+      );
+      const maxTempFahrenheit = Math.round(
+        json.forecast.forecastday[i].day.maxtemp_f
+      );
+      const minTempFahrenheit = Math.round(
+        json.forecast.forecastday[i].day.mintemp_f
+      );
 
       // Log data to console
-      console.log(
-        `${dayOfWeek}: ${maxTempCelsius}°C ${maxTempFahrenheit}°F, ${minTempCelsius}°C ${minTempFahrenheit}°F`
-      );
+      //   console.log(
+      //     `${dayOfWeek}: ${maxTempCelsius}°C ${maxTempFahrenheit}°F, ${minTempCelsius}°C ${minTempFahrenheit}°F`
+      //   );
+
+      // Add weather for the current day to the array of objects
+      const obj = {};
+
+      obj[`Day of the Week`] = dayOfWeek;
+      obj["maxTempCelsius"] = maxTempCelsius;
+      obj["minTempCelsius"] = minTempCelsius;
+      obj["maxTempFahrenheit"] = maxTempFahrenheit;
+      obj["minTempFahrenheit"] = minTempFahrenheit;
+
+      WeatherDailyLibrary.push(obj);
     }
+    return WeatherDailyLibrary;
   } catch (error) {
     console.log(`${error}`);
     console.log("Could not find the searched location");
   }
 }
 
-// Function invocations
-getWeatherToday(`Montreal`);
-getWeatherForecastedTrihoral(`Montreal`);
-getWeatherForecastedDay(`Montreal`);
+// Location input form event handler function
+async function inputFormHandler(event) {
+  if (event.key === "Enter") {
+    const location = searchForm.value;
+    searchForm.value = "";
+    // Wrap these functions in a promise.all function. Also, the functions need to return objects with the required data,
+    // and the promise.all function will return an array of all of the required data?
+
+    const promise1 = getWeatherToday(location);
+    const promise2 = getWeatherForecastedTrihoral(location);
+    const promise3 = getWeatherForecastedDay(location);
+
+    // Resolve promises and finish with an array of processed data
+    const results = await Promise.all([promise1, promise2, promise3]);
+    console.log("SUCCESS!!");
+    console.log(results);
+  }
+}
+
+// Input form event listener
+let searchForm = document.querySelector("input");
+searchForm.addEventListener("keypress", inputFormHandler);
